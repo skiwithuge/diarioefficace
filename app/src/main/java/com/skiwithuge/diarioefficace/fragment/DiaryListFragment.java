d88a0244c68d9adbacc3590e8d8ba2393c83ee26
skiwithuge <rasciky@gmail.com>
2016-12-05 10:08:05 +0100

    Adapter Working Version

--- app/src/main/java/com/skiwithuge/diarioefficace/fragment/DiaryListFragment.java
+++ app/src/main/java/com/skiwithuge/diarioefficace/fragment/DiaryListFragment.java
@@ -1,14 +1,22 @@
 package com.skiwithuge.diarioefficace.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.skiwithuge.diarioefficace.R;
+import com.skiwithuge.diarioefficace.adapter.DayAdapter;
+import com.skiwithuge.diarioefficace.model.Day;
+import com.skiwithuge.diarioefficace.model.DayList;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by skiwithuge on 11/19/16.
@@ -17,7 +25,7 @@ import com.skiwithuge.diarioefficace.R;
 public class DiaryListFragment extends Fragment {
 
     private RecyclerView mDayRecyclerView;
-
+    private DayAdapter mAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +41,39 @@ public class DiaryListFragment extends Fragment {
                 .findViewById(R.id.day_recycler_view);
         mDayRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
 
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    private void updateUI() {
+        //UUID id = UUID.randomUUID();
+        //Day day = new Day(id);
+        //DayList.get(getActivity()).addDay(day);
+
+
+        DayList dl = DayList.get(getActivity());
+        List<Day> days = dl.getDays();
+
+        if (mAdapter == null) {
+            mAdapter = new DayAdapter(getActivity(), days);
+            mDayRecyclerView.setAdapter(mAdapter);
+
+        } else {
+            mAdapter.setDays(days);
+            mAdapter.notifyDataSetChanged();
+        }
+
+    }
+
 }
